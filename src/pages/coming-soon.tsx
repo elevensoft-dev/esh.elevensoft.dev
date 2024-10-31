76b5a8d8ec60f37587690f236ac6589b5482bbf6
João Küster <kusterzito@gmail.com>
2024-10-31 10:19:11 -0300

    feat(ui): add footer

--- src/pages/coming-soon.tsx
+++ src/pages/coming-soon.tsx
@@ -6,20 +6,18 @@ export function ComingSoon() {
         alt="Letter E"
         className="fixed inset-0 w-full h-full opacity-50 z-10"
       />
-      <img
-        src="assets/logo-light.png"
-        alt="logo"
-        className="z-20 text-white"
-        width={250}
-        height={100}
-      />
-      <h1 className="text-xl sm:text-5xl font-bold text-white z-20">
-        Estamos chegando muito em breve
+
+      <h1 className="flex flex-col text-xl sm:text-5xl font-bold text-white z-20 gap-y-5">
+        <span className="text-orange-600 text-6xl">ESH</span>
+        <span className="leading-tight">
+          A maneira mais simples e segura de acessar e proteger sua
+          infraestrutura
+        </span>
       </h1>
       <p className="text-base text-slate-50 max-w-lg z-20">
-        Transparência em cada comando. <br></br>{" "}
-        <span className="font-bold text-orange-500">ESH:</span> Segurança
-        inteligente para suas equipes.
+        Transparência em cada comando. <br></br> Segurança inteligente para suas
+        equipes. <br></br>
+        Disponível muito em breve.
       </p>
       <a
         aria-label="contact via whatsapp button"
@@ -29,6 +27,16 @@ export function ComingSoon() {
       >
         Entrar em contato
       </a>
+      <footer className="fixed bottom-0 py-5 bg-neutral-900 inset-x-0 flex items-center justify-center gap-x-3 z-20">
+        <span className="text-white text-sm opacity-50">Um produto </span>
+        <img
+          src="assets/logo-light.png"
+          alt="logo"
+          className="z-20 text-white"
+          width={100}
+          height={100}
+        />
+      </footer>
     </section>
   );
 }
