302703268756f67cd5ed94cde99fe9d7b602c9ae
João Küster <kusterzito@gmail.com>
2024-11-01 17:33:39 -0300

    feat(ui): router fixes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import router from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 );
