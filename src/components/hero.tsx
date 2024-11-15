8e2c54c30dc1c58ff22412a0e7b5af9146d8d6c3
João Küster <kusterzito@gmail.com>
2024-11-15 14:41:32 -0300

    feat(ui): change video player

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import { PlayCircle } from "lucide-react";
-import Plyr from "plyr-react";
-import "plyr-react/plyr.css";
+import { Loader2Icon } from "lucide-react";
+import ReactPlayer from "react-player";
 
 function Hero() {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const handlePlayVideo = () => {
-    // Slight delay for smoother transition
-    setTimeout(() => {
-      setIsPlaying(true);
-    }, 100);
-  };
-
   return (
     <section className="pt-24 pb-16 px-4 sm:pt-32 sm:pb-24 bg-gradient-to-b from-gray-50 to-white relative">
       <div className="max-w-7xl mx-auto text-center z-50">
@@ -25,49 +14,46 @@ function Hero() {
           segura de sessões e monitoramento em tempo real.
         </p>
         <div className="flex justify-center space-x-4 mb-12">
-          <button className="bg-orange-600 text-white px-8 py-3 rounded-lg hover:bg-orange-700 transition-colors flex items-center"
-            onClick={handlePlayVideo}
+          <a
+            target="_blank"
+            href="https://wa.me/message/X2DMDA457ASDN1"
+            className="cursor-pointer bg-orange-600 text-white px-8 py-3 rounded-lg hover:bg-orange-700 transition-colors flex items-center"
           >
-            <PlayCircle className="mr-2 h-5 w-5" />
-            Assistir Vídeo
-          </button>
-          <a href="#features" className="bg-white text-orange-600 border-2 border-orange-600 px-8 py-3 rounded-lg hover:bg-orange-50 transition-colors">
+            Quero contratar
+          </a>
+          <a
+            href="#features"
+            className="bg-white text-orange-600 border-2 border-orange-600 px-8 py-3 rounded-lg hover:bg-orange-50 transition-colors"
+          >
             Saber mais
           </a>
         </div>
-        <div className="relative max-w-4xl mx-auto">
+        <div className="relative max-w-4xl mx-auto h-full">
           <div className="aspect-video rounded-xl overflow-hidden shadow-2xl border-8 border-white">
-              <div className="w-full h-full">
-                <Plyr
-                  hidden = {!isPlaying}
-                  source={{
-                    type: "video",
-                    sources: [
-                      {
-                        src: "1029769804",
-                        provider: "vimeo",
-                      },
-                    ],
-                  }}
-                  options={{
-                    autoplay: true,
-                    muted: true,
-                  }}
-                />
-              </div>
-              <div className="relative w-full h-full" hidden={isPlaying}>
-                <img
-                  src="/assets/esh_thumb.png"
-                  alt="ESH Dashboard Preview"
-                  className="w-full h-full object-cover"
-                />
-                <div
-                  className="absolute inset-0 bg-black/40 flex items-center justify-center cursor-pointer"
-                  onClick={handlePlayVideo}
-                >
-                  <PlayCircle className="w-16 h-16 text-white hover:scale-110 transition-transform" />
-                </div>
-              </div>
+            <div className="relative max-w-4xl mx-auto h-full">
+              <ReactPlayer
+                controls
+                pip
+                url="https://vimeo.com/1029769804"
+                playing={true}
+                config={{
+                  vimeo: {
+                    title: "ESH",
+                    playerOptions: {
+                      url: "1029769804",
+                    },
+                  },
+                }}
+                light="/assets/esh_thumb.png"
+                width={"100%"}
+                height={500}
+                fallback={
+                  <div className="w-full h-full absolute mx-auto inset-0 bg-muted flex items-center justify-center">
+                    <Loader2Icon className="animate animate-spin text-orange-600 w-32 h-32" />
+                  </div>
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -76,4 +62,3 @@ function Hero() {
 }
 
 export default Hero;
-
