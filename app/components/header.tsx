1cec8a41e738a3b450c24cd1246d276b79f2a517
João Kuster <kusterzito@gmail.com>
2025-04-25 18:29:49 -0300

    feat(ui): header and hero section

---
+++ app/components/header.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+
+export function Header() {
+  return (
+    <header className="sticky left-0 top-0 z-100 px-5 flex w-full flex-col border-b border-border border-neutral-800">
+      <div className="flex h-16 bg-surface-primary dark:bg-dark-surface-primary">
+        <div className="items-center container mx-auto flex justify-between w-full">
+          <h1 className="text-3xl font-semibold">
+            <span className="text-orange-400">E</span>
+SH
+          </h1>
+          <div className="flex items-center gap-10">
+            <p>Features</p>
+            <Link to="/">Preços</Link>
+            <Link to="/">Blog</Link>
+            <Link to="/">Changelog</Link>
+          </div>
+          <div className="flex items-center gap-2">
+            <Link to="/" className="rounded-full bg-orange-400 text-white text-sm px-4 py-2 font-light">Começar hoje</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
