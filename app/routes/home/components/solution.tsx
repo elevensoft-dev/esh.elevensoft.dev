b7f6319d7158c45ed3ea37de2ffebf0ffade811b
Joao Kuster <kusterzito@gmail.com>
2025-11-13 10:41:08 -0300

    fix: remove fox icon from website. change fox icon to fresh system picture. minified old images sizes, 4MB reduced from images bundle size.

--- app/routes/home/components/solution.tsx
+++ app/routes/home/components/solution.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon, IdCardIcon } from '@radix-ui/react-icons';
+import { CheckIcon, IdCardIcon } from "@radix-ui/react-icons"
 
 export function Solution() {
   return (
@@ -15,54 +15,94 @@ export function Solution() {
           Chega de torcer para não dar errado.
         </h1>
         <p className="text-neutral-400 text-lg mb-10 max-w-3xl mx-auto">
-          Tenha a prova irrefutável de cada ação e previna incidentes antes que eles aconteçam.
+          Tenha a prova irrefutável de cada ação e previna incidentes antes que
+          eles aconteçam.
         </p>
 
-        <div className='space-y-12 flex flex-col items-center w-full'>
-
+        <div className="space-y-12 flex flex-col items-center w-full">
           <article className="bg-neutral-900 p-3 flex-col md:flex-row rounded-xl ring-1 ring-neutral-800 flex gap-x-5 w-full">
-            <div className='text-start space-y-5 md:w-1/2 p-6'>
-              <h2 className='text-xl md:text-3xl font-medium text-white'>Responda a Qualquer Auditoria em Minutos</h2>
-              <ul className='flex flex-col gap-y-5'>
+            <div className="text-start space-y-5 md:w-1/2 p-6">
+              <h2 className="text-xl md:text-3xl font-medium text-white">
+                Responda a Qualquer Auditoria em Minutos
+              </h2>
+              <ul className="flex flex-col gap-y-5">
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-neutral-400' /></div>
-                  <p className='text-neutral-400'>
-                  Saiba exatamente <b>quem</b> acessou, <b>quando</b> e <b>o que</b> fez.
+                  <div className="rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-neutral-400" />
+                  </div>
+                  <p className="text-neutral-400">
+                    Saiba exatamente <b>quem</b> acessou, <b>quando</b> e{" "}
+                    <b>o que</b> fez.
                   </p>
                 </li>
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-neutral-400' /></div>
-                  <p className='text-neutral-400'>Com o <b>replay visual completo</b> de cada sessão privilegiada.</p>
+                  <div className="rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-neutral-400" />
+                  </div>
+                  <p className="text-neutral-400">
+                    Com o <b>replay visual completo</b> de cada sessão
+                    privilegiada.
+                  </p>
                 </li>
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-neutral-400' /></div>
-                  <p className='text-neutral-400'>Responda a <b>qualquer auditoria</b> em minutos, não em dias, e atenda a normas como <b>LGPD</b> e <b>ISO 27001</b>. sem estresse.</p>
+                  <div className="rounded-full w-6 h-6 bg-neutral-800 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-neutral-400" />
+                  </div>
+                  <p className="text-neutral-400">
+                    Responda a <b>qualquer auditoria</b> em minutos, não em
+                    dias, e atenda a normas como <b>LGPD</b> e <b>ISO 27001</b>.
+                    sem estresse.
+                  </p>
                 </li>
               </ul>
             </div>
-            <div className='md:w-1/2 h-80 overflow-hidden ring-1 ring-neutral-800 rounded-xl bg-neutral-950'>
-              <img className="object-cover object-center w-full h-full" src="/dashboard-notebook.webp" alt="Dashboard do ESH" />
+            <div className="md:w-1/2 h-80 overflow-hidden ring-1 ring-neutral-800 rounded-xl bg-neutral-950">
+              <img
+                className="object-cover object-center w-full h-full"
+                src="grupo-servidores-notebook.png"
+                alt="Dashboard do ESH"
+              />
             </div>
           </article>
 
           <article className="bg-orange-950/20 p-3 rounded-xl ring-2 ring-orange-500/50 flex flex-col md:flex-row gap-x-5 w-full">
-            <div className='md:w-1/2 h-80 overflow-hidden ring-1 ring-neutral-800 rounded-xl bg-neutral-950'>
-              <img className="object-cover object-center w-full h-full" src="/tags-notebook.webp" alt="Tela de permissões por tags no ESH" />
+            <div className="md:w-1/2 h-80 overflow-hidden ring-1 ring-neutral-800 rounded-xl bg-neutral-950">
+              <img
+                className="object-cover object-center w-full h-full"
+                src="tags-notebook.webp"
+                alt="Tela de permissões por tags no ESH"
+              />
             </div>
-            <div className='text-start space-y-5 md:w-1/2 p-6'>
-              <h2 className='text-xl md:text-3xl font-medium text-orange-300'>Vá Além da Auditoria. Impeça o Risco.</h2>
-              <ul className='flex flex-col gap-y-5'>
+            <div className="text-start space-y-5 md:w-1/2 p-6">
+              <h2 className="text-xl md:text-3xl font-medium text-orange-300">
+                Vá Além da Auditoria. Impeça o Risco.
+              </h2>
+              <ul className="flex flex-col gap-y-5">
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-orange-300' /></div>
-                  <p className='text-neutral-300'>Auditoria reativa é olhar o passado. <b>Controle preventivo é proteger o futuro.</b></p>
+                  <div className="rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-orange-300" />
+                  </div>
+                  <p className="text-neutral-300">
+                    Auditoria reativa é olhar o passado.{" "}
+                    <b>Controle preventivo é proteger o futuro.</b>
+                  </p>
                 </li>
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-orange-300' /></div>
-                  <p className='text-neutral-300'>Bloqueie comandos <b>perigosos</b> como <code>rm -rf</code>.</p>
+                  <div className="rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-orange-300" />
+                  </div>
+                  <p className="text-neutral-300">
+                    Bloqueie comandos <b>perigosos</b> como <code>rm -rf</code>.
+                  </p>
                 </li>
                 <li className="w-full flex items-center gap-x-3">
-                  <div className='rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center'><CheckIcon className='w-4 h-4 text-orange-300' /></div>
-                  <p className='text-neutral-300'>Implemente o <strong>Princípio do Menor Privilégio</strong> de forma intuitiva.</p>
+                  <div className="rounded-full w-6 h-6 bg-orange-400/20 flex items-center justify-center">
+                    <CheckIcon className="w-4 h-4 text-orange-300" />
+                  </div>
+                  <p className="text-neutral-300">
+                    Implemente o <strong>Princípio do Menor Privilégio</strong>{" "}
+                    de forma intuitiva.
+                  </p>
                 </li>
               </ul>
             </div>
